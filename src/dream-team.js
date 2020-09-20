0b2d8974f2531c383de4e0e20b9a989768832143
Yakubov Kirill <yakubov.kirill.work@gmail.com>
2020-09-20 12:27:21 +0300

    feat: complete Dream_team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  return members.reduce((dreanTeam, elem) => {
+      if (typeof elem !== "string") return dreanTeam += '';
+      let trimStr = elem.trim();
+      return dreanTeam += trimStr[0].toUpperCase();
+  }, '')
+      .split('')
+      .sort()
+      .join('');
 };
