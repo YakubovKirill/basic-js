6a92e52eb782b73346fbbf022d27a82cfddbfb5e
Yakubov Kirill <yakubov.kirill.work@gmail.com>
2020-09-20 11:13:12 +0300

    feat: create count cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+    let catsCount = 0;
+    if (matrix.length === 0) return 0;
+    return matrix.reduce((count, line) => {
+        return count += line.filter(element => `${element}` === '^^').length;
+    }, 0);
 };
