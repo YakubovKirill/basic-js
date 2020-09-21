ba93abc238444308aa215fa30eb01a776e1d01cf
Yakubov Kirill <yakubov.kirill.work@gmail.com>
2020-09-22 01:07:06 +0300

    feat: complete extended_repeated task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+
+module.exports = function repeater(str, options = {...parametres}) {
+  if (options.repeatTimes === undefined) options.repeatTimes = 1;
+  if (options.additionRepeatTimes === undefined) options.additionRepeatTimes = 1;
+  let opt = {
+    repeatTimes: options.repeatTimes || 1,
+    separator: options.separator || '+',
+    addition: ((typeof options.addition === "boolean") || (options.addition === null)) ? `${options.addition}`: options.addition || '',
+    additionRepeatTimes: options.additionRepeatTimes || 0,
+    additionSeparator: options.additionSeparator || '|'
+  }
+
+  let getAdditions = (options) => {
+    if (options.additionRepeatTimes === 0) return '';
+    if (options.additionRepeatTimes === 1) return `${options.addition}`;
+    let additionStr = '';
+    for(let i = 1; i <= options.additionRepeatTimes - 1; i++) {
+      additionStr += `${options.addition}${options.additionSeparator}`;
+    }
+    additionStr += `${options.addition}`;
+    return additionStr;
+  }
+  
+  if (opt.repeatTimes === 0) return '';
+  if (opt.repeatTimes === 1) return `${str}${getAdditions(opt)}`;
+  let finalStr = ``;
+  for(let i = 1; i <= opt.repeatTimes - 1; i++) {
+    finalStr += `${str}${getAdditions(opt)}${opt.separator}`;
+  }
+  finalStr += `${str}${getAdditions(opt)}`;
+  return finalStr;
 };
   
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,38 @@
 const CustomError = require("../extensions/custom-error");
-
-const chainMaker = {
+let chainMaker = {
+  chain : new Array(),
   getLength() {
     throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    //return this.chain.length;
   },
-  addLink(value) {
+  addLink(value = '') {
     throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    /*
+    this.chain.push(`( ${value} )`)
+    return this;
+    */
   },
   removeLink(position) {
     throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    /*
+    if ((position < 1) || (position > this.chain.length)) throw new Error()
+    this.chain = this.chain.filter(elem => elem !== this.chain[position - 1]);
+    return this;
+    */
   },
   reverseChain() {
     throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    /*
+    this.chain = this.chain.reverse()
+    return this;
+    */
   },
   finishChain() {
     throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    /*
+    this.chain = this.chain.join('~~')
+    return this.chain;
+    */
   }
 };
 
