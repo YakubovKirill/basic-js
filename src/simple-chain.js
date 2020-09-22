beba3a579d9d65b9779efbbf15ee446d768cb41a
Yakubov Kirill <yakubov.kirill.work@gmail.com>
2020-09-22 23:48:28 +0300

    feat: complete simple_chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,38 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 let chainMaker = {
-  chain : new Array(),
+  chain : [],
   getLength() {
-    throw new CustomError('Not implemented');
-    //return this.chain.length;
+    return this.chain.length;
   },
   addLink(value = '') {
-    throw new CustomError('Not implemented');
-    /*
     this.chain.push(`( ${value} )`)
     return this;
-    */
+    
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    /*
-    if ((position < 1) || (position > this.chain.length)) throw new Error()
+    if ((position < 1) || (position > this.chain.length)) {
+      this.chain = [];
+      throw new Error()
+    }
     this.chain = this.chain.filter(elem => elem !== this.chain[position - 1]);
     return this;
-    */
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    /*
     this.chain = this.chain.reverse()
     return this;
-    */
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    /*
-    this.chain = this.chain.join('~~')
-    return this.chain;
-    */
+    let finalStr = this.chain.join('~~');
+    this.chain = [];
+    return finalStr;
   }
 };
 
