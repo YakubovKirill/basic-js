2f4054694b42c3af790d98a25789dbbac5393f4e
Yakubov Kirill <yakubov.kirill.work@gmail.com>
2020-09-22 22:48:13 +0300

    feat: complete vengere_cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,67 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  decryptor;
+  constructor (type = true) {
+    if (typeof type !== "boolean") throw new Error();
+    this.decryptor = type; 
+  }
+  
+  encrypt(str, keyword) {
+    if ((typeof str !== "string") && (typeof keyword !== "string")) throw new Error();
+
+    let basicStr = str.toUpperCase().split('');
+    let key = this.getKey(basicStr, keyword);
+    let finalStr = '';
+
+    basicStr.forEach((elem, index) => {
+      if (this.isLetter(elem)) {
+        let alphabetElemIndex = (elem.charCodeAt(0) + key.charCodeAt(index)) % this.alphabet.length;
+        finalStr += this.alphabet[alphabetElemIndex]
+      }
+      else finalStr += elem;
+    });
+
+    return (this.decryptor) ? finalStr : finalStr.split('').reverse().join('');
+  }   
+  decrypt(str, keyword) {
+    if ((typeof str !== "string") && (typeof keyword !== "string")) throw new Error();
+
+    let basicStr = str.toUpperCase().split('');
+    let key = this.getKey(basicStr, keyword);
+    let finalStr = '';
+
+    basicStr.forEach((elem, index) => {
+      if (this.isLetter(elem)) {
+        let alphabetElemIndex = (elem.charCodeAt(0) + this.alphabet.length - key.charCodeAt(index)) % this.alphabet.length;
+        finalStr += this.alphabet[alphabetElemIndex]
+      }
+      else finalStr += elem;
+    });
+
+    return (this.decryptor) ? finalStr : finalStr.split('').reverse().join('');
+  }
+
+  isLetter(char) {
+    let length = this.alphabet.filter(elem => elem === char).length;
+    return (length === 0) ? false : true;
+  }
+
+  getKey (strArr, keyword) {
+    let key = '';
+    let counter = 0;
+
+    strArr.forEach(element => {
+      if (counter === keyword.length) counter = 0;
+      if (this.isLetter(element)) {
+        key += keyword[counter].toUpperCase();
+        counter ++;
+      }
+      else key += element;
+    });
+
+    return key;
   }
 }
 
