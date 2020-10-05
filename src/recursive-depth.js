9856870e93b1d98df9bb8cff63a699648054ef4f
Yakubov Kirill <yakubov.kirill.work@gmail.com>
2020-10-05 18:58:43 +0300

    feat: create depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,7 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(arr) {
-    throw new CustomError('Not implemented');
+  calculateDepth(arr, counter = 1, arrCounter = []) {
+    arrCounter.push(counter);
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        this.calculateDepth(arr[i], counter + 1, arrCounter)
+      }
+    }
+    let result = Math.max(...arrCounter);
+    return result;
   }
 };
+
+let result;
